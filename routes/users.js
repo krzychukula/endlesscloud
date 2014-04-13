1f18b66263f46a0abb57ba5e6669adfdf49eec5c
Krzysztof Kula <krzychukula@gmail.com>
2014-04-13 20:22:55 +0200

    list values from table

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var conString = process.env.DATABASE_URL || "postgres://endless:1234@localhost/cloud";
 
-
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM your_table', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
+var getFromDB = function(callback){
+  var client = new pg.Client(conString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query('select * from activity;', function(err, result) {
+      if(err) {
+        console.error('error running query', err);
+      }
+      console.log(result);
+      //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+      client.end();
+      callback(err, result);
     });
   });
+}
 
-});
+var getActivities = function(req, res) {
+  getFromDB(function(err, result){
+    res.render('activities', { title: 'Express', activities: result.rows });
+  })
+};
+
+/* GET users listing. */
+router.get('/', getActivities);
 
 module.exports = router;
