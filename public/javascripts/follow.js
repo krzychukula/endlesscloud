84433b2b140b4dfb7cad1f9bbadb49d7434e10af
Krzysztof Kula <krzychukula@gmail.com>
2014-04-23 21:28:14 +0200

    after seeing Game Mechanic Explorer decided to go Phaser

---
+++ public/javascripts/follow.js
@@ -0,0 +1,96 @@
+// This example uses the Phaser 2.0.4 framework
+// /http://gamemechanicexplorer.com/#follow-1
+// Copyright © 2014 John Watson
+// Licensed under the terms of the MIT License
+
+var GameState = function(game) {
+};
+
+// Load images and sounds
+GameState.prototype.preload = function() {
+    this.game.load.image('player', 'assets/gfx/player.png');
+};
+
+// Setup the example
+GameState.prototype.create = function() {
+    // Set stage background color
+    this.game.stage.backgroundColor = 0x4488cc;
+
+    this.player = new Follower(this.game, this.game.width/2, this.game.height/2, this.game.input)
+
+    // Create a follower
+    this.game.add.existing(this.player);
+
+    // Simulate a pointer click/tap input at the center of the stage
+    // when the example begins running.
+    this.game.input.x = this.game.width/2;
+    this.game.input.y = this.game.height/2;
+
+    // Enable physics on the player
+    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+    // Make player collide with world boundaries so he doesn't leave the stage
+    this.player.body.collideWorldBounds = true;
+
+    // Show FPS
+    this.game.time.advancedTiming = true;
+    this.fpsText = this.game.add.text(
+        20, 20, '', { font: '16px Arial', fill: '#ffffff' }
+    );
+};
+
+// The update() method is called every frame
+GameState.prototype.update = function() {
+    if (this.game.time.fps !== 0) {
+        this.fpsText.setText(this.game.time.fps + ' FPS x:'+game.input.x+' y:'+game.input.y);
+    }
+};
+
+// Follower constructor
+var Follower = function(game, x, y, target) {
+    Phaser.Sprite.call(this, game, x, y, 'player');
+
+    // Save the target that this Follower will follow
+    // The target is any object with x and y properties
+    this.target = target;
+
+    // Set the pivot point for this sprite to the center
+    this.anchor.setTo(0.5, 0.5);
+
+    // Enable physics on this object
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    // Define constants that affect motion
+    this.MAX_SPEED = 30; // pixels/second
+    this.MIN_DISTANCE = 32; // pixels
+};
+
+// Followers are a type of Phaser.Sprite
+Follower.prototype = Object.create(Phaser.Sprite.prototype);
+Follower.prototype.constructor = Follower;
+
+Follower.prototype.update = function() {
+    // Calculate distance to target
+    var distance = this.game.math.distance(this.x, this.y, this.target.x, this.target.y);
+
+    // If the distance > MIN_DISTANCE then move
+    if (distance > this.MIN_DISTANCE) {
+        // Calculate the angle to the target
+
+        //why not working? http://gamemechanicexplorer.com/#follow-1
+
+        // var rotation = this.game.math.angleBetween(this.x, this.y, this.target.x, this.target.y);
+
+        // // Calculate velocity vector based on rotation and this.MAX_SPEED
+        // this.body.velocity.x = Math.cos(rotation) * this.MAX_SPEED;
+        // this.body.velocity.y = Math.sin(rotation) * this.MAX_SPEED;
+
+        this.game.physics.arcade.moveToPointer(this, 300);
+
+    } else {
+        this.body.velocity.setTo(0, 0);
+    }
+};
+
+var game = new Phaser.Game(848, 450, Phaser.AUTO, 'game');
+game.state.add('game', GameState, true);
