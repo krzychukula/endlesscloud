a4ac7d0beebe3488cec017cf81002b43a12f17c2
krzychukula <krzychukula@gmail.com>
2016-04-20 21:08:58 +0200

    experiment with browserify and glslify + more standard

--- public/javascripts/webgl-build.js
+++ public/javascripts/webgl-build.js
@@ -1,8 +1,9 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 // index.js
-var glslify = require('glslify')
 
-var src = glslify(__dirname + '/../shader.glsl')
+
+var src = "precision mediump float;\n#define GLSLIFY 1\n\nvoid main() {\n  gl_FragColor = vec4(1.0);\n}\n"
 
 console.log(src)
 
@@ -122,3 +123,5 @@ var main = function () {
 document.addEventListener('DOMContentLoaded', function (event) {
   main()
 })
+
+},{}]},{},[1]);
